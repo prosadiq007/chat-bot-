c2381e3b5254e9059701f59b8d32ca9b1309a40f
J MD Sadiqullah <prosadiq007@gmail.com>
2026-09-24 21:48:34 +0530

    Update chatbott.py

--- chatbott.py
+++ chatbott.py
@@ -78,7 +78,7 @@ if "messages" not in st.session_state:
 # ============================================================
 
 st.markdown(
-    '<div class="title">🤖 College Assistant Chatbot</div>',
+    '<div class="title">🤖 IntelliChat Chatbot</div>',
     unsafe_allow_html=True
 )
 
